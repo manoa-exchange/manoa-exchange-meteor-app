a6cc08964bdb8b6eb430b10b598e3928c00c01be
josiahkila <127445228+josiahkila@users.noreply.github.com>
2023-11-09 15:43:36 -1000

    1

--- app/imports/ui/components/PostItem.jsx
+++ app/imports/ui/components/PostItem.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { Image } from 'react-bootstrap';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-const PostItem = ({ post }) => (
-  <tr>
-    <td>{post.name}</td>
-    <td>{post.quantity}</td>
-    <td>{post.image}</td>
-    <td>{post.caption}</td>
-    <td>
-      <Link to={`/edit/${post._id}`}>Edit</Link>
-    </td>
-  </tr>
-);
+const PostItem = ({ post }) => {
+  const { currentUser } = useTracker(() => ({
+    currentUser: Meteor.user() ? Meteor.user().username : '',
+  }), []);
+
+  return (
+    <tr>
+      <td>{currentUser}</td>
+      <td>{post.quantity}</td>
+      <td><Image src={post.image} width={75} /></td>
+      <td>{post.caption}</td>
+      <td>
+        <Link to={`/edit/${post._id}`}>Edit</Link>
+      </td>
+    </tr>
+  );
+};
 
 // Require a document to be passed to this component.
 PostItem.propTypes = {
--- app/imports/ui/pages/AddPost.jsx
+++ app/imports/ui/pages/AddPost.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
 import { Card, Col, Container, Row } from 'react-bootstrap';
-import { AutoForm, ErrorsField, NumField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import { AutoForm, ErrorsField, NumField, TextField, SubmitField } from 'uniforms-bootstrap5';
 import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
-import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import { Posts } from '../../api/post/Post';
 
-// Create a schema to specify the structure of the data to appear in the form.
-const formSchema = new SimpleSchema({
-  name: String,
-  quantity: Number,
-  image: String,
-  caption: String,
-});
-
-const bridge = new SimpleSchema2Bridge(formSchema);
-
-/* Renders the AddPost page for adding a document. */
 const AddPost = () => {
+  const [initialValues, setInitialValues] = useState({});
+  const { userProfile } = useTracker(() => {
+    const user = Meteor.user();
+    const userProfile = user ? { firstname: user.profile.firstname, lastname: user.profile.lastname } : {};
+    return {
+      userProfile
+    };
+  }, []);
+
+  useEffect(() => {
+    if (userProfile.firstname && userProfile.lastname) {
+      setInitialValues({ name: `${userProfile.firstname} ${userProfile.lastname}` });
+    }
+  }, [userProfile]);
+
+  const formSchema = new SimpleSchema({
+    name: String,
+    quantity: Number,
+    image: String,
+    caption: String,
+  });
+
+  const bridge = new SimpleSchema2Bridge(formSchema);
 
   // On submit, insert the data.
   const submit = (data, formRef) => {
@@ -37,17 +50,16 @@ const AddPost = () => {
     );
   };
 
-  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
   let fRef = null;
   return (
     <Container className="py-3">
       <Row className="justify-content-center">
         <Col xs={5}>
-          <Col className="text-center"><h2>Add Post</h2></Col>
-          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)}>
+          <h2 className="text-center">Add Post</h2>
+          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} model={initialValues} onSubmit={data => submit(data, fRef)}>
             <Card>
               <Card.Body>
-                <TextField name="name" />
+                <TextField name="name" readOnly />
                 <NumField name="quantity" decimal={null} />
                 <TextField name="image" />
                 <TextField name="caption" />
