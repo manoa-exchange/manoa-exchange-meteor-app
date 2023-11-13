7ce9a0e3ed6efbabdd95c87c6ed7486e3e369c76
Kelly Sato <ksato7@hawaii.edu>
2023-11-13 13:14:57 -1000

    Update Carousel.jsx

--- app/imports/ui/components/Carousel.jsx
+++ app/imports/ui/components/Carousel.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -15,10 +14,10 @@ const DynamicCarousel = () => {
   };
 
   const ResourceItems = [
-    { title: 'MIX', description: 'Manoa International Exchange', link: 'https://manoa.hawaii.edu/global/study-abroad-and-exchange/', image: '' },
-    { title: 'SAC', description: 'Study Abroad Center', link: 'https://manoa.hawaii.edu/global/study-abroad-and-exchange/', image: '' },
-    { title: 'NSE', description: 'National Student Exchange', link: 'https://manoa.hawaii.edu/global/study-abroad-and-exchange/', image: '' },
-    { title: 'NSE', description: 'National Student Exchange', link: 'https://manoa.hawaii.edu/global/study-abroad-and-exchange/', image: '' },
+    { title: 'MIX', description: 'Manoa International Exchange', link: 'http://manoa.hawaii.edu/mix/', image: '' },
+    { title: 'SAC', description: 'Study Abroad Center', link: 'http://www.studyabroad.hawaii.edu/', image: '' },
+    { title: 'NSE', description: 'National Student Exchange', link: 'http://www.studyabroad.hawaii.edu/', image: '' },
+    { title: 'Study Abroad and Exchange', description: 'UHM overseas options for current students and post-graduates', link: 'https://manoa.hawaii.edu/global/study-abroad-and-exchange/', image: '' },
     { title: 'NSE', description: 'National Student Exchange', link: 'https://manoa.hawaii.edu/global/study-abroad-and-exchange/', image: '' },
   ];
 
