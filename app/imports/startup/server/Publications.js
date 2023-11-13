c9d227842187542fae8c3ffa9cf4d17591ae97d5
RichardBzar <richardbtzar@gmail.com>
2023-11-13 13:00:36 -1000

    added stuff back to publications

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,10 +1,17 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
+import { Stuffs } from '../../api/stuff/Stuff.js';
 import { Profiles } from '../../api/profile/Profile';
-
 
 // User-level publication.
 // If logged in, then publish documents owned by this user. Otherwise, publish nothing.
+Meteor.publish(Stuffs.userPublicationName, function () {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return Stuffs.collection.find({ owner: username });
+  }
+  return this.ready();
+});
 
 // User level publication for Profile Collection
 Meteor.publish(Profiles.userPublicationName, function () {
@@ -17,7 +24,12 @@ Meteor.publish(Profiles.userPublicationName, function () {
 
 // Admin-level publication.
 // If logged in and with admin role, then publish all documents from all users. Otherwise, publish nothing.
-
+Meteor.publish(Stuffs.adminPublicationName, function () {
+  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
+    return Stuffs.collection.find();
+  }
+  return this.ready();
+});
 
 // Admin level publication for profile collection
 Meteor.publish(Profiles.adminPublicationName, function () {
