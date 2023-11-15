afd8e9be997b1747805d8b4b4b919bf8da10134d
Josiah K Kila <127445228+josiahkila@users.noreply.github.com>
2023-11-14 14:27:07 -1000

    Add files via upload

---
+++ app/imports/ui/components/NavBar.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
+import { NavLink } from 'react-router-dom';
+import { Roles } from 'meteor/alanning:roles';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { BoxArrowRight, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
+
+const NavBar = () => {
+  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+  const { currentUser } = useTracker(() => ({
+    currentUser: Meteor.user() ? Meteor.user().username : '',
+  }), []);
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand as={NavLink} to="/">
+          <h2>MĀNOA EXCHANGE</h2>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto justify-content-start">
+            {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
+              <Nav.Link id="list-stuff-admin-nav" as={NavLink} to="/admin" key="admin">Admin</Nav.Link>
+            ) : ''}
+          </Nav>
+          <Nav className="justify-content-end">
+            {currentUser ? ([
+              <Nav.Link id="create-nav" as={NavLink} to="/create" key="create">Create</Nav.Link>,
+              <Nav.Link id="posts-nav" as={NavLink} to="/posts" key="posts">Posts</Nav.Link>,
+              <Nav.Link id="profile-nav" as={NavLink} to="/profile" key="profiles">Profile</Nav.Link>,
+            ]) : ''}
+            {currentUser === '' ? (
+              <NavDropdown id="login-dropdown" title="Sign In">
+                <NavDropdown.Item id="login-dropdown-sign-in" as={NavLink} to="/signin">
+                  <PersonFill />
+                  Sign in
+                </NavDropdown.Item>
+                <NavDropdown.Item id="login-dropdown-sign-up" as={NavLink} to="/signup">
+                  <PersonPlusFill />
+                  Register
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <NavDropdown id="navbar-current-user" title={currentUser}>
+                <NavDropdown.Item id="navbar-sign-out" as={NavLink} to="/signout">
+                  <BoxArrowRight />
+                  {' '}
+                  Sign out
+                </NavDropdown.Item>
+              </NavDropdown>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
---
+++ app/imports/ui/pages/ListPostAdmin.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Col, Container, Row, Table } from 'react-bootstrap';
+import { Posts } from '../../api/post/Post';
+import PostItemAdmin from '../components/PostItemAdmin';
+import LoadingSpinner from '../components/LoadingSpinner';
+
+/* Renders a table containing all of the Stuff documents. Use <PostItemAdmin.jsx> to render each row. */
+const ListPostAdmin = () => {
+  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+  const { stuffs, ready } = useTracker(() => {
+    // Get access to Stuff documents.
+    const subscription = Meteor.subscribe(Posts.adminPublicationName);
+    // Determine if the subscription is ready
+    const rdy = subscription.ready();
+    // Get the Stuff documents
+    const items = Posts.collection.find({}).fetch();
+    return {
+      posts: items,
+      ready: rdy,
+    };
+  }, []);
+  return (ready ? (
+    <Container className="py-3">
+      <Row className="justify-content-center">
+        <Col md={7}>
+          <Col className="text-center"><h2>List Post (Admin)</h2></Col>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Quantity</th>
+                <th>Image</th>
+                <th>Caption</th>
+                <th>Owner</th>
+              </tr>
+            </thead>
+            <tbody>
+              {stuffs.map((stuff) => <PostItemAdmin key={stuff._id} stuff={stuff} />)}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
+  ) : <LoadingSpinner />);
+};
+
+export default ListPostAdmin;
