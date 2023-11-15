e4fcbfc4e5c2d017100548ade773249cd23c4288
Josiah K Kila <127445228+josiahkila@users.noreply.github.com>
2023-11-14 14:25:31 -1000

    Add files via upload

---
+++ app/imports/imports/api/post/Post.js
@@ -0,0 +1,37 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+/**
+ * The PostsCollection. It encapsulates state and variable values for stuff.
+ */
+class PostsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'PostsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      name: String,
+      owner: String,
+      image: String,
+      caption: String,
+      likeCount: {
+        type: Number,
+        optional: true, // Make likeCount optional
+        defaultValue: 0, // You can also set a default value
+      },
+    });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+/**
+ * The singleton instance of the PostsCollection.
+ * @type {PostsCollection}
+ */
+export const Posts = new PostsCollection();
