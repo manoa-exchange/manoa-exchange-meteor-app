e44cf8be2877e50cb27a7fb8a04c1c7c754570f1
Kelly Sato <ksato7@hawaii.edu>
2023-11-21 12:47:06 -1000

    Update style.css

--- app/imports/ui/components/LandingNavBar.jsx
+++ app/imports/ui/components/LandingNavBar.jsx
@@ -6,7 +6,7 @@ const LandingNav = () => (
   <Navbar expand="lg">
     <Container fluid className="px-5">
       <Navbar.Brand as={NavLink} to="/" key="landing">
-        <Image src="../images/MElogo.png" alt="manoa-exchange-logo" height={75} />
+        <Image src="../images/ME-logo-white.png" alt="manoa-exchange-logo" height={75} />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
@@ -14,7 +14,7 @@ const LandingNav = () => (
           <Nav.Link className="ps-3" as={NavLink} to="#resources" key="resources">Resources</Nav.Link>
           <Nav.Link className="ps-3" as={NavLink} to="/signin" key="signin">Sign In</Nav.Link>
           <Nav.Item className="ps-3">
-            <Button href="/signup" variant="outline-dark">Register</Button>
+            <Button href="/signup" className="outline-light">Register</Button>
           </Nav.Item>
         </Nav>
       </Navbar.Collapse>
