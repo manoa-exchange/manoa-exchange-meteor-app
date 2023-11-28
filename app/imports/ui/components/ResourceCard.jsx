de7e33b6ad6b9d3ca905dbd7fe59223f26dfb64d
RichardBzar <richardbtzar@gmail.com>
2023-11-27 14:13:28 -1000

    Revert "Merge branch 'main' into issue-23"
    This reverts commit fac8d7aa9880ed9453ac4b805cacab20ffe06887, reversing
    changes made to e338e201beb59ec8ada4c480a29fd30863b02df8.

--- app/imports/ui/components/ResourceCard.jsx
+++ app/imports/ui/components/ResourceCard.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import { Button, Card, Container } from 'react-bootstrap';
+import { Card, Container } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const ResourceCard = ({ title, link }) => (
   <Container>
-    <Card className="pt-2 pb-2" style={{ height: '200px' }}>
-      <Card.Body className="d-flex flex-column justify-content-center">
-        <div style={{ minHeight: '100px' }}>
-          <Card.Title className="mb-0 text-center">
-            <Container className="d-flex align-items-center justify-content-center text-center">
-              <h5>{title}</h5>
-            </Container>
-          </Card.Title>
-        </div>
-        <div style={{ minHeight: '50px' }}>
-          <Container className="d-flex align-items-center justify-content-center">
-            <Button href={link} className="outline-dark">Visit Site →</Button>
+    <Card style={{ height: '200px' }}>
+      <Card.Header>
+        <Card.Title>
+          <Container className="d-flex align-items-center justify-content-center text-center pt-3">
+            <h5>{title}</h5>
           </Container>
-        </div>
-      </Card.Body>
+        </Card.Title>
+      </Card.Header>
+      <Card.Footer>
+        <Link to={link}><p>Visit external site →</p></Link>
+      </Card.Footer>
     </Card>
   </Container>
 );
