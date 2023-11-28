7f5b794fb2b521a46d1f97467387462825843730
Kayla-Marie Torres <ktorres2@hawaii.edu>
2023-11-28 12:54:29 -1000

    reroute post page to home page

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -50,8 +50,8 @@ const App = () => {
           <Route path="/signout" element={<SignOut />} />
           <Route path="/cloudinarypage" element={<CloudinaryPage />} />
           <Route path="/uploadwidget" element={<UploadWidget />} />
-          <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-          <Route path="/posts" element={<ProtectedRoute><ListPost /></ProtectedRoute>} />
+          <Route path="/home" element={<ProtectedRoute><ListPost /></ProtectedRoute>} />
+          <Route path="/post" element={<ProtectedRoute><ListPost /></ProtectedRoute>} />
           <Route path="/savedposts" element={<ProtectedRoute><ListSavedPost /></ProtectedRoute>} />
           <Route path="/create" element={<ProtectedRoute><AddPost /></ProtectedRoute>} />
           <Route path="/edit/:_id" element={<ProtectedRoute><EditPost /></ProtectedRoute>} />
