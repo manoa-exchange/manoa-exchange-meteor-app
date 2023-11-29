f681c2a51a2eb37c06679b25a92b152c15f97093
Kelly Sato <ksato7@hawaii.edu>
2023-11-28 23:07:36 -1000

    Update AddPost.jsx

--- app/imports/ui/pages/AddPost.jsx
+++ app/imports/ui/pages/AddPost.jsx
@@ -1,4 +1,4 @@
-/* global Meteor */
+import { Meteor } from 'meteor/meteor';
 import React, { useState, useEffect, useRef } from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
 import { AutoForm, ErrorsField, TextField, SubmitField } from 'uniforms-bootstrap5';
