871f6f9ad2e416b0bd78e1f438014cd34179e290
Kelly Sato <ksato7@hawaii.edu>
2023-12-05 13:00:17 -1000

    Update

--- app/imports/ui/components/Search.jsx
+++ app/imports/ui/components/Search.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import { Container, Form, Button } from 'react-bootstrap';
+import { Container, Form } from 'react-bootstrap';
 
-const Search = () => {
-  const tags = 
-
-  return (
-    <Container>
-      <Form.Control type="search" placeholder="Search tags..." />
-      <Container>
-        <Button>Japan</Button>
-      </Container>
-    </Container>
-  );
-};
+const Search = () => (
+  <Container>
+    <Form.Control type="search" placeholder="Search tags..." />
+  </Container>
+);
 
 export default Search;
