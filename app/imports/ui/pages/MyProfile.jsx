6df7fbb64db61c3a5ecc2de297dc7a5306266a9a
RichardBzar <richardbtzar@gmail.com>
2023-12-09 23:48:39 -1000

    update myprofile, and signup to have profilecollection insert and subscription

--- app/imports/ui/pages/MyProfile.jsx
+++ app/imports/ui/pages/MyProfile.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Card, Col, Container, ListGroup, Row } from 'react-bootstrap';
+import { Button, Card, Col, Container, ListGroup, Row } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { Profiles } from '../../api/profile/Profile';
 import { PageIDs } from '../utilities/ids';
+/** Renders a color-blocked static landing page. */
 import { Posts } from '../../api/post/Post';
 import { Comments } from '../../api/comment/Comment';
 import PostItem from '../components/PostItem';
@@ -35,7 +36,7 @@ const MyProfile = () => {
       <Container className="mb-4">
         <Row className="mt-4">
           <Col lg={4}>
-            <Card className="rounded border border-dark card_profile">
+            <Card className="mb-4 rounded border border-dark card_profile">
               <Card.Body className="text-center">
                 <Card.Img
                   /* eslint-disable-next-line max-len */
@@ -47,6 +48,10 @@ const MyProfile = () => {
                 />
                 <h3 className="mb-1 mt-3">{userProfile?.firstName || 'Insert Name'} {userProfile?.lastName || 'Insert Name'}</h3>
                 <p className="text-muted mb-4">{userProfile.idNumber}</p>
+                <p className="text-muted mb-4">Insert Campus/Location</p>
+                <div className="d-flex justify-content-center mb-2">
+                  <Button>Follow</Button>
+                </div>
               </Card.Body>
             </Card>
             <Card className="mb-4 mt-4 border border-dark">
@@ -68,6 +73,45 @@ const MyProfile = () => {
                     <Card.Text className="text-muted">example@example.com</Card.Text>
                   </Col>
                 </Row>
+                <hr />
+                <Row>
+                  <Col sm="3">
+                    <Card.Text>Phone</Card.Text>
+                  </Col>
+                  <Col sm="9">
+                    <Card.Text className="text-muted">(097) 234-5678</Card.Text>
+                  </Col>
+                </Row>
+                <hr />
+                <Row>
+                  <Col sm="3">
+                    <Card.Text>Mobile</Card.Text>
+                  </Col>
+                  <Col sm="9">
+                    <Card.Text className="text-muted">(098) 765-4321</Card.Text>
+                  </Col>
+                </Row>
+                <hr />
+                <Row>
+                  <Col sm="3">
+                    <Card.Text>Address</Card.Text>
+                  </Col>
+                  <Col sm="9">
+                    <Card.Text className="text-muted">Bay Area, San Francisco, CA</Card.Text>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+            <Card className="mb-4 mb-lg-0 card border border-dark red">
+              <Card.Body className="p-0 card1">
+                <ListGroup flush className="rounded-3 red">
+                  <ListGroup.Item className="d-flex justify-content-between align-items-center p-3 red">
+                    <Card.Text>Insert Number Of Posts Made</Card.Text>
+                  </ListGroup.Item>
+                  <ListGroup.Item className="d-flex justify-content-between align-items-center p-3 red">
+                    <Card.Text>Insert Number Of Posts Favorited</Card.Text>
+                  </ListGroup.Item>
+                </ListGroup>
               </Card.Body>
             </Card>
           </Col>
