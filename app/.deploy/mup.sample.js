0163bd44c3d5b425932faf9ffeef0424736c079f
Kelly Sato <ksato7@hawaii.edu>
2023-12-11 16:49:50 -1000

    Update mup.sample.js

--- app/.deploy/mup.sample.js
+++ app/.deploy/mup.sample.js
@@ -1,20 +1,20 @@
 module.exports = {
   servers: {
     one: {
-      host: '111.222.333.444',
+      host: 'manoaexchange.com',
       username: 'root',
-      password: 'changeme'
+      password: '38Spec!al'
     }
   },
   app: {
     // if you edit the app 'name' field, be sure to run 'mup stop' if the app is already running.
     // otherwise you will have two apps deployed at once, with unpredictable results.
-    name: 'meteor-react-bootstrap-template',
+    name: 'manoa-exchange',
     path: '../',
     servers: { one: {}, },
     buildOptions: { serverOnly: true },
     env: {
-      ROOT_URL: 'http://111.222.333.444',
+      ROOT_URL: 'https://manoaexchange.com',
       MONGO_URL: 'mongodb://mongodb/meteor',
       MONGO_OPLOG_URL: 'mongodb://mongodb/local',
     },
@@ -22,5 +22,12 @@ module.exports = {
     enableUploadProgressBar: true
   },
   mongo: { version: '5.0', servers: { one: {} }
+  },
+  proxy: {
+    domains: 'manoaexchange.com',
+    ssl: {
+      letsEncryptEmail: 'ksato7@hawaii.edu',
+      forceSSL: true
+    }
   },
 };
